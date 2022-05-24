8162194c4e624e4b3bd6e7febcbee4b5c5fbba05
Erick Recher <deathofasellout@yahoo.com>
2022-05-24 15:19:06 -0700

    working

--- problems/04-range.js
+++ problems/04-range.js
@@ -19,15 +19,14 @@ let range = (start, end) => {
       return;
     } else {
       answer.push(start);
-      console.log(start);
       return solve(start + 1, end);
     }
   }
   solve(start,end);
   return answer;
 }
-console.log(range(1, 5)); // [1, 2, 3, 4]
-console.log(range(3, 4)); // [3]
+// console.log(range(1, 5)); // [1, 2, 3, 4]
+// console.log(range(3, 4)); // [3]
 // range(7, 6); // []
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -12,8 +12,22 @@ reverse("id"); // "di"
 reverse(""); // ""
 ***********************************************************************/
 
-// your code here
-
+let reverse = string => {
+  if(string.length <= 0 || string === "") {
+    return "";
+  } else {
+    let strArr = string.split("");
+    let lastLetter = strArr.pop();
+    let newStr = strArr.join("");
+    return lastLetter + reverse(newStr);
+  }
+}
+// reverse("house"); // "esuoh"
+// reverse("dog"); // "god"
+// reverse("atom"); // "mota"
+// reverse("q"); // "q"
+//console.log(reverse("id")); // "di"
+//reverse(""); // ""
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = reverse;
