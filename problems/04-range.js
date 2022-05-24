6ae8c0756b3c7bd40188488e3992d9e95406dbc7
Erick Recher <deathofasellout@yahoo.com>
2022-05-24 14:17:36 -0700

    First Commit

---
+++ problems/04-range.js
@@ -0,0 +1,38 @@
+/***********************************************************************
+Write a recursive function, `range`, that takes a start and an end and returns
+an array of all numbers in that range, exclusive. If the end number is less than
+the start, return an empty array.
+
+Examples:
+***********************************************************************/
+// range(1, 5); // [1, 2, 3, 4]
+// range(3, 4); // [3]
+// range(7, 6); // []
+
+let range = (start, end) => {
+  let answer = []
+
+  function solve(start, end){
+    if (end < start) {
+      return answer;
+    } else if(start === end) {
+      return;
+    } else {
+      answer.push(start);
+      console.log(start);
+      return solve(start + 1, end);
+    }
+  }
+  solve(start,end);
+  return answer;
+}
+console.log(range(1, 5)); // [1, 2, 3, 4]
+console.log(range(3, 4)); // [3]
+// range(7, 6); // []
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = range;
+} catch (e) {
+  module.exports = null;
+}
